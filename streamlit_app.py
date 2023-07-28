e89dc01ee8333b8d4967d69c52ee5d7faece6adf
ElyssaJoy08 <140700677+ElyssaJoy08@users.noreply.github.com>
2023-07-28 17:42:26 +0800

    Update streamlit_app.py
    rephrase command text

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ streamlit.write('The user entered ', fruit_choice)
 
 
 
-streamlit.header("The fruit load list contains:")
+streamlit.header("View Our Fruit List - Add Your Favorites!")
 # Snowflake-related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
